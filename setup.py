3d7fa98424290c7f421c7190958c451bf7d76891
andylockran <andy@zrmt.com>
2015-12-09 11:36:44 +0000

    Aded v6 with connection

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'heatmiserV3',
   packages = ['heatmiserV3'], # this must be the same as the name above
-  version = '0.5',
+  version = '0.6',
   description = 'A library to interact with Heatmiser Themostats using the v3 protocol',
   author = 'Andy Loughran',
   author_email = 'andy@zrmt.com',
   url = 'https://github.com/andylockran/heatmiserV3', # use the URL to the github repo
-  download_url = 'https://github.com/andylockran/heatmiserV3/tarball/0.5', # I'll explain this in a second
+  download_url = 'https://github.com/andylockran/heatmiserV3/tarball/0.6', # I'll explain this in a second
   keywords = ['v3', 'thermostat', 'heatmiser'], # arbitrary keywords
   classifiers = [],
   install_requires = ['pyserial']
