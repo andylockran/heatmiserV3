b51d5e828255c35b72e04ae19dad219c3961298c
andylockran <andy@zrmt.com>
2015-12-09 10:28:59 +0000

    Latest 0.3 build

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'heatmiserV3',
   packages = ['heatmiserV3'], # this must be the same as the name above
-  version = '0.2',
+  version = '0.3',
   description = 'A library to interact with Heatmiser Themostats using the v3 protocol',
   author = 'Andy Loughran',
   author_email = 'andy@zrmt.com',
   url = 'https://github.com/andylockran/heatmiserV3', # use the URL to the github repo
-  download_url = 'https://github.com/andylockran/heatmiserV3/tarball/0.2', # I'll explain this in a second
+  download_url = 'https://github.com/andylockran/heatmiserV3/tarball/0.3', # I'll explain this in a second
   keywords = ['v3', 'thermostat', 'heatmiser'], # arbitrary keywords
   classifiers = [],
 )
