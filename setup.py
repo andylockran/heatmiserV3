1bea01532301cd88ecfb901fa08a7fb827cda617
Andy Loughran <andy@zrmt.com>
2019-01-20 19:34:16 +0000

    Eurgh, I hate config.yml

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from setuptools import setup
 setup(
     name='heatmiserV3',
     packages=['heatmiserV3'],  # this must be the same as the name above
-    version='1.1.5',
+    version='1.1.6',
     description='A library to interact with Heatmiser Themostats using V3',
     author='Andy Loughran',
     author_email='andy@zrmt.com',
     tests_require=['pytest', 'pylint', 'flake8'],
-    data_files = [('config', ['config/config.yml'])],
+    data_files = [('config', ['heatmiserV3/config.yml'])],
     include_package_data=True ,
     url='https://github.com/andylockran/heatmiserV3',
     download_url='https://github.com/andylockran/heatmiserV3/tarball/1.1.0',
