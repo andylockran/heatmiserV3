bc62806b096d7633b179aa43b261b17e07860184
Andy Loughran <andy@lockran.com>
2019-11-30 14:26:58 +0000

    Updated version 1.1.12 is the same as 1.1.13

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup(
     name='heatmiserV3',
     packages=['heatmiserV3'],  # this must be the same as the name above
-    version='1.1.11',
+    version='1.1.13',
     description='A library to interact with Heatmiser Themostats using V3',
     author='Andy Loughran',
     author_email='andy@zrmt.com',
