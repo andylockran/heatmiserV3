298700f0a83a678cd3b3c5928b71306d35314fbf
Andy Loughran <andy@lockran.com>
2024-02-04 20:04:16 +0000

    Cleaned up schema uri

--- heatmiserv3/thermostat.py
+++ heatmiserv3/thermostat.py
@@ -13,6 +13,8 @@ logging.basicConfig(
 MockSerialThermostat = MockSerial()
 MockSerialThermostat.open()
 
+
+
 def interpret_message_generate_response(message):
     """ 
     Calculates the CRC of the message 
@@ -35,7 +37,6 @@ def interpret_message_generate_response(message):
     response[9:73] = bytearray([0,64,0,15,2,0,2,1,0,0,0,1,0,0,0,20,0,15,21,28,1,1,0,0,0,0,0,0,255,255,255,255,0,201,0,0,4,16,22,18,24,0,19,24,0,5,24,0,19,24,0,5,5,30,23,22,0,20,24,0,16,24,0,16]) ## Contents
     data = list(response)
     data = data + crc.run(data)
-
     checksum = data[len(data) - 2:]
     logging.info(f"Checksum value is: {checksum}")
     rxmsg = data[: len(data) - 2]
@@ -48,20 +49,18 @@ def interpret_message_generate_response(message):
 ## Mock Thermostat 1
 MockSerialThermostat.stub(
     receive_bytes=b'\x01\n\x81\x00\x00\x00\xff\xff,\t',
-    send_bytes=interpret_message_generate_response(b'\x01\n\x81\x00\x00\x00\xff\xff,\t,\xd4\x8d')
+    send_bytes=interpret_message_generate_response(b'\x01\n\x81\x00\x00\x00\xff\xff,\t')
     # send_bytes=calculate_crc_in_bytearray(b'\x01\n\x81\x00\x00\x00\xff\xff,\t')
 )
 
 ## Mock Thermostat 2
 MockSerialThermostat.stub(
-    receive_bytes=b'\x02\n\x81\x00\x00\x00\xff\xff,\t',
-    send_bytes=interpret_message_generate_response(b'\x02\n\x81\x00\x00\x00\xff\xff,\t,\xd4\x8d')
-    # send_bytes=calculate_crc_in_bytearray(b'\x01\n\x81\x00\x00\x00\xff\xff,\t')
+    receive_bytes=b'\x02\n\x81\x00\x00\x00\xff\xffY\xc1',
+    send_bytes=interpret_message_generate_response(b'\x02\n\x81\x00\x00\x00\xff\xffY\xc1')
 )
 
 # Mock Thermostat 3
 MockSerialThermostat.stub(
     receive_bytes=b'\x03\n\x81\x00\x00\x00\xff\xff\x8a\x86',
     send_bytes=interpret_message_generate_response(b'\x03\n\x81\x00\x00\x00\xff\xff,\t,\xd4\x8d')
-    # send_bytes=calculate_crc_in_bytearray(b'\x01\n\x81\x00\x00\x00\xff\xff,\t')
 )
